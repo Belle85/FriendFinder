411609e7437d763ad0fee3cffe6b95a05cbfde28
Belle <bebold.bebelle@gmail.com>
2018-09-19 12:53:58 -0400

    Completed styling. Began with the javascri for the submit button.

---
+++ app/data/friends.js
@@ -0,0 +1,33 @@
+var friendArray = [
+    {
+    name:"Kristen Bell",
+    photo:"http://www.babynowusa.com/nyc/wp-content/uploads/2015/04/kristen-bell-and-dax-shepard-couples-therapy-is-the-key-to-our-healthy-marriage1.jpg",
+    scores:[
+        5,1,5,5,5,1,2,5,4,1
+      ]
+  },
+  {
+    name:"Emma Watson ",
+    photo: "https://imgix.ranker.com/user_node_img/50056/1001108809/original/j-k-rowling-knew-she-was-hermione-from-the-beginning-photo-u1?w=650&q=50&fm=jpg&fit=crop&crop=faces",
+    scores:[
+        5,3,4,3,5,3,3,5,4,3
+      ]
+  },
+  {
+    name:"Dax Sheppard",
+    photo:"https://pbs.twimg.com/profile_images/796807040408522752/_s4cs8ZF.jpg",
+    scores:[
+        5,1,4,4,5,1,2,5,4,1
+      ]
+  },
+  {
+    name:"Leonardo DiCaprio",
+    photo:"https://media1.popsugar-assets.com/files/thumbor/DTkaL_bUZ-O8oiClCNREFENNBPc/fit-in/2048xorig/filters:format_auto-!!-:strip_icc-!!-/2016/04/13/757/n/1922398/562e8d42_edit_img_image_16360412_1460565518/i/Leonardo-DiCaprio-Cute-Moments.png",
+    scores:[
+        2,1,3,4,3,5,2,1,4,1
+      ]
+  }
+
+];
+//export array//
+module.exports = friendArray;
